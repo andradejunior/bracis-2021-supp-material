f8a5b38ee18d30e9ac8a5efa115207b3fc3ad0d6
Jonathan Cardoso-Silva <jonathan.car.silva@gmail.com>
2022-02-24 16:37:42 +0000

    Update README

--- setup.sh
+++ setup.sh
@@ -10,3 +10,12 @@ echo "c.MappingKernelManager.default_kernel_name='env'" >> env/share/jupyter/lab
 pip install -r requirements.txt
 
 jupyter nbextension enable --py widgetsnbextension
+
+wget https://developer.download.nvidia.com/compute/cuda/repos/wsl-ubuntu/x86_64/cuda-wsl-ubuntu.pin
+sudo mv cuda-wsl-ubuntu.pin /etc/apt/preferences.d/cuda-repository-pin-600
+wget https://developer.download.nvidia.com/compute/cuda/11.6.1/local_installers/cuda-repo-wsl-ubuntu-11-6-local_11.6.1-1_amd64.deb
+sudo dpkg -i cuda-repo-wsl-ubuntu-11-6-local_11.6.1-1_amd64.deb
+sudo apt-key add /var/cuda-repo-wsl-ubuntu-11-6-local/7fa2af80.pub
+sudo apt-get update
+sudo apt-get -y install cuda
+sudo dpkg -i cuda-repo-wsl-ubuntu-11-6-local_11.6.1-1_amd64.deb
